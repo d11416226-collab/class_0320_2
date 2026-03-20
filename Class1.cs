6755af7959cea3be8f039d3adf766c138fea4ba5
d11416226-collab <d11416226@gs.takming.edu.tw>
2026-03-20 11:30:47 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Test
+{
+	public int Mid;
+	private int Final;
+
+	public void SetFinal(int value)
+	{
+		Final = value;
+	}
+
+	public int GetFinal()
+	{
+		return Final;
+	}
+	public double GetAvg()
+	{
+		return (Mid + Final)/2.0;
+	}
+	public void SetGrade( int m , int f)
+	{
+		if (ValidGrade(m,f))
+		{
+			Mid = m;
+			Final = f;
+		}
+	}
+
+	private bool ValidGrade(int m , int f )
+	{
+		if ( m >=0  && m <= 100)
+		{
+			if ( f >=0 && f <=100)
+			{ return true; 
+			}
+			else
+			{
+				return false; 
+			} 
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace class_0320_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1, std2;
+            std1 = new Test();
+            std2 = new Test();
+
+            std1.Mid = 78;
+            std1.SetFinal(72);
+
+            std2.SetGrade(-68,102);
+
+
+            Console.WriteLine("第一位" +std1.GetAvg().ToString());
+
+            Console.WriteLine("第二位" +std2.GetAvg().ToString());
+
+        }
+    }
+}
